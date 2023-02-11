0686c360d928feb6e1a36ac183e2af8d3fc62c60
Monika Mali <monamali2682@gmail.com>
2023-02-11 20:06:05 +0530

    initial commit

---
+++ main.java
@@ -0,0 +1,9 @@
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("hi, monika this side");
+        int a = 23;
+        int b =34;
+        System.out.println(sum.getSum(a,b));
+    }
+}
---
+++ sum.java
@@ -0,0 +1,5 @@
+public class sum {
+    public static int getSum(int a, int b){
+        return a+b;
+    }
+}
